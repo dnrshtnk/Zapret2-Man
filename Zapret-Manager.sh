3a406852f5342cbbbe3be561168e4bc60aa2d3f5
Munez <128070378+dnrshtnk@users.noreply.github.com>
2026-02-28 18:46:46 +0300

    Add files via upload

--- Zapret-Manager.sh
+++ Zapret-Manager.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # ==========================================
-# Zapret on remittor Manager by StressOzz
+# Zapret
 # =========================================
-ZAPRET_MANAGER_VERSION="9.1"; ZAPRET_VERSION="72.20260226"; ZAPRET2_VERSION="0.9.20260226"; STR_VERSION_AUTOINSTALL="v1"
+ZAPRET_MANAGER_VERSION="0.1"; ZAPRET_VERSION="72.20260226"; ZAPRET2_VERSION="0.9.20260226"; STR_VERSION_AUTOINSTALL="v1"
 TEST_HOST="https://rr1---sn-gvnuxaxjvh-jx3z.googlevideo.com"; LAN_IP=$(uci get network.lan.ipaddr 2>/dev/null | cut -d/ -f1)
 GREEN="\033[1;32m"; RED="\033[1;31m"; CYAN="\033[1;36m"; YELLOW="\033[1;33m"; MAGENTA="\033[1;35m"; BLUE="\033[0;34m"; NC="\033[0m"; DGRAY="\033[38;5;244m"
 CONF="/etc/config/zapret"; CUSTOM_DIR="/opt/zapret/init.d/openwrt/custom.d/"; HOSTLIST_FILE="/opt/zapret/ipset/zapret-hosts-user.txt"
